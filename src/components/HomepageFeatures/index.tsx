be4316069eea1b2a29283ed5bfb041675b9cacfb
Cristiano <jcristiano.c@gmail.com>
2023-09-19 15:39:08 -0300

    Novos topicos

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -59,11 +59,7 @@ export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+        
       </div>
     </section>
   );
